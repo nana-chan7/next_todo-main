b1121a0e79e83d3ba8077107b2d8dc98b2823154
frogBear_7 <{220098tt@yse-c.net}>
2024-02-07 11:05:52 +0900

    up

--- app/services/TodoService.ts
+++ app/services/TodoService.ts
@@ -1,10 +1,11 @@
 import { Todo } from "../models/Todo";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "/api/todo";
 
 export const getTodos = async () => {
     //TODO: API URL設定
-    const url = API_URL + "";
+    const url = API_URL + "/get";
+    // const url = `${API_URL}/get`;
     try {
         const response = await fetch(url);
         if (response.ok) {
@@ -18,7 +19,23 @@ export const getTodos = async () => {
 export const postTodos = async (todos: Todo[]) => {
     if (!todos) return;
     //TODO: API URL設定
-    const url = API_URL + "";
+    const url = API_URL + "/add";
+    // const url = `${API_URL}/add`; 
     const data = JSON.stringify(todos);
     //TODO: APIで保存し、データを返す
+    try {
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: data,
+        });
+        if (!response.ok) {
+            throw new Error('Failed to post todos');
+        }
+        return await response.json(); 
+    } catch (error) {
+        console.error(error);
+    }
 }
